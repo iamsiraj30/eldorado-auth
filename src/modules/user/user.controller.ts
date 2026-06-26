98118c4d50cb2909583e721e313da82ab8751e0f
iamsiraj30 <sirajul.islam1@softvence.com>
2026-06-26 12:45:19 +0600

    role base assecc implemented

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from 'src/common/enums/role.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 export type IUser = {
   id: string;
   name: string;
@@ -11,4 +15,11 @@ export type IUser = {
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Get('/admin')
+  getAdmin() {
+    return 'admin only';
+  }
 }
