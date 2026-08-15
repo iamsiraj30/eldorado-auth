c62a1e4d38e0ae5a69a50a05919048f49364eae8
iamsiraj30 <sirajul.islam1@softvence.com>
2026-08-15 11:07:31 +0600

    fix: update jwt canActive interface

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -1,19 +1,32 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { Role } from 'src/common/enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles =
+      this.reflector.getAllAndOverride<Role[]>(
+        ROLES_KEY,
+        [
+          context.getHandler(),
+          context.getClass(),
+        ],
+      );
 
-    if (!requiredRoles) {
+    // No role restriction
+    if (!requiredRoles?.length) {
       return true;
     }
 
@@ -21,6 +34,12 @@ export class RolesGuard implements CanActivate {
 
     const user = request.user;
 
-    return requiredRoles.includes(user.role as Role);
+    if (!user) {
+      throw new UnauthorizedException(
+        'Please Login First',
+      );
+    }
+
+    return requiredRoles.includes(user.role);
   }
 }
